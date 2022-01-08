2b62b5d49789c4e2228039eb97b6562394346c02
SHISHU <shishus808@gmail.com>
2022-01-08 23:34:11 +0530

    all bugs remobved

--- coronaTracker/src/api/fun/ApiFun.java
+++ coronaTracker/src/api/fun/ApiFun.java
@@ -4,7 +4,6 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
